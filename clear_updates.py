803ffff6bf064639ff53db927573f21381a4999a
King-0-D <ohianidominic04@gmail.com>
2026-02-08 22:43:55 +0100

    Delete clear_updates.py

--- clear_updates.py
+++
@@ -1,9 +0,0 @@
-from telegram import Bot
-import os
-
-TOKEN = os.environ.get("BOT_TOKEN")
-bot = Bot(TOKEN)
-
-# Delete all pending updates
-bot.delete_webhook(drop_pending_updates=True)
-print("Cleared old updates. You can now run the bot safely.")
