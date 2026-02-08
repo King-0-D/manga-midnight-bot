7d2b6f31d9a9ec3ecb3418ec07cd4f6ea5bfd038
King-0-D <ohianidominic04@gmail.com>
2026-02-08 21:09:42 +0100

    Add bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TOKEN = os.environ.get("8234896869:AAEzjw83bEln6TqqlVis-YmTwRC8e38ETfk")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Manga Midnight bot is alive.")
+
+async def ping(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("pong")
+
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("ping", ping))
+
+    app.run_polling()
