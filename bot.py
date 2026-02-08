322ae3c7527075bf230fe3683b3b91cbc3d684f2
King-0-D <ohianidominic04@gmail.com>
2026-02-08 21:54:42 +0100

    bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("8234896869:AAEzjw83bEln6TqqlVis-YmTwRC8e38ETfk")
+TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Manga Midnight bot is alive.")
